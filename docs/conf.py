0af4fb577e24d9955df8fe76a1fe459b3d4b95e8
Filip Sedivy <mail@filipsedivy.cz>
2026-04-18 15:12:53 +0200

    Add copyright information to Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 project = "goojprt-pt210-sdk"
 author = "Filip Sedivy"
+copyright = "2026, Filip Sedivy"
 release = "0.1.0"
 
 extensions = [
