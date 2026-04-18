29d97ab91789fc6ea76d65d19510e3b6def777b6
Filip Sedivy <mail@filipsedivy.cz>
2026-04-18 14:56:03 +0200

    Add Sphinx configuration for project documentation

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = "goojprt-pt210-sdk"
+author = "Filip Sedivy"
+release = "0.1.0"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+html_theme = "alabaster"
