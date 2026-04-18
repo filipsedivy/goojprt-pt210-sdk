0e9730348cea88686227841f5cdc5f2781bb5b96
Filip Sedivy <mail@filipsedivy.cz>
2026-04-18 15:09:28 +0200

    Update Sphinx path configuration to use `os.path.join` for improved compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 project = "goojprt-pt210-sdk"
 author = "Filip Sedivy"
